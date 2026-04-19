f8ee75aaeb2cd9a9174288a4bd6e466009d79f71
Sameer <sameer.adnan@proton.me>
2026-04-19 16:31:01 +0500

    data types

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let x = 4;
     println!("x is: {}", x);
 
+    let arr = [1, 2, 3, 4, 5]; // arrays always have to be same data type unlike tuple
+
     {
         let x = x - 2;
         println!("x is: {}", x);
@@ -15,4 +17,14 @@ fn main() {
 
     const SECONDS_IN_MINUTE: u32 = 60;
     println!("{}", SECONDS_IN_MINUTE);
+
+    let mut tup = (1, true, "hello");
+
+    tup.1 = false;
+
+    println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
+
+    println!("{}", arr[1]);
 }

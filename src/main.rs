26338fb62f331ded122b7e8cb7d9528455be4936
Sameer <sameer.adnan@proton.me>
2026-04-19 12:30:24 +0500

    variables constants and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 4;
+    println!("x is: {}", x);
+
+    {
+        let x = x - 2;
+        println!("x is: {}", x);
+    }
+
+    let x = x + 1;
+    println!("x is: {}", x);
+
+    let x = "hello";
+    println!("x is: {}", x);
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
